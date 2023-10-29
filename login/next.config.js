2da96c9230ef20e3583e170f24796fa4e4e3a45e
ArturGZ <arturo.quintana.016@gmail.com>
2023-10-28 21:20:30 -0600

    Added files for deployment on container and k8s

--- login/next.config.js
+++ login/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
 const nextConfig = {
-    // output: 'export',
+    // reactSrictMode: true,
+    // webpack(config, options){
+    //     config.plugins.push(
+    //         new NextFederationPlugin({
+    //             name: 'login',
+    //             filename: 'static/chunks/remoteEntry.js',
+    //             remotes: {
+    //                tasks:'tasks@http://localhost:3001/_next/static/chunks/remoteEntry.js',
+    //             },
+    //         })
+    //     );
+    //     return config;
+    // },
+    distDir: 'out',
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
