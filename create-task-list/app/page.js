a30b6819abd5a8ef1655afd5dd5f71fb41c50b7c
arturo0510 <gonzalez.arturo3c@gmail.com>
2023-10-30 18:31:49 -0600

    Added responsive grid for dynamic screen change

--- create-task-list/app/page.js
+++ create-task-list/app/page.js
@@ -1,11 +1,11 @@
-'use client';
+'use client'; 
 
 import React, { useState } from 'react';
-import { Button, TextField, Typography, FormControl, InputLabel, Select, MenuItem, Paper, Box } from '@mui/material';
+import { Button, TextField, Typography, FormControl, InputLabel, Select, MenuItem, Paper, Box, Grid } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/theme';
 
-const listaTareas = () => {
+const ListaTareas = () => {
   const [nombreLista, setNombreLista] = useState('');
   const [descripcionLista, setDescripcionLista] = useState('');
   const [colorLista, setColorLista] = useState('');
@@ -31,48 +31,51 @@ const listaTareas = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Paper elevation={3} style={{ padding: '10px', maxWidth: '700px', margin: '0 auto', marginTop: '40px' }}>
-        <Typography variant="h1" pt = {5} color="initial"></Typography>
-        <Typography variant="h2" sx={{ ...theme.titleText }} align="center">Crear Lista de Tareas</Typography>
-        <Typography variant="h5" sx={{ ...theme.normalText }} align="center" pt = {4}>Nombre de la Lista:</Typography>
-        <TextField
-          sx={{ ...theme.container }}
-          fullWidth
-          value={nombreLista}
-          onChange={handleNombreChange}
-        />
-        <Typography variant="h5" sx={{ ...theme.normalText }} align="center" pt = {4}>Descripción:</Typography>
-        <TextField
-          sx={{ ...theme.container }}
-          multiline
-          maxRows={3}
-          fullWidth
-          value={descripcionLista}
-          onChange={handleDescripcionChange}
-        />
-        <Typography variant="h5" sx={{ ...theme.normalText }} align="center" pt = {4}>Color de la Lista:</Typography>
-        <FormControl fullWidth sx={{ ...theme.container }}>
-          <Select
-            value={colorLista}
-            onChange={handleColorChange}
-          >
-            <MenuItem value="red">Rojo</MenuItem>
-            <MenuItem value="blue">Azul</MenuItem>
-            <MenuItem value="green">Verde</MenuItem>
-            <MenuItem value="yellow">Amarillo</MenuItem>
-          </Select>
-        </FormControl>
-        <Box display="flex" justifyContent="space-between" style={{ marginTop: '30px', marginBottom: '15px' }}>
-          <Button variant="contained" color="primary" size="large" onClick={handleSubmit}>
-            Crear Lista de Tareas
-          </Button>
-          <Button variant="contained" color="error" size="large">
-            Cancelar
-          </Button>
-        </Box>
-      </Paper>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12} sm={10} md={8} lg={6}>
+          <Paper elevation={3} style={{ padding: '10px' }}>
+            <Typography variant="h2" sx={{ ...theme.titleText }} align="center">Crear Lista de Tareas</Typography>
+            <Typography variant="h5" sx={{ ...theme.normalText }} align="center">Nombre de la Lista:</Typography>
+            <TextField
+              sx={{ ...theme.container }}
+              fullWidth
+              value={nombreLista}
+              onChange={handleNombreChange}
+            />
+            <Typography variant="h5" sx={{ ...theme.normalText }} align="center">Descripción:</Typography>
+            <TextField
+              sx={{ ...theme.container }}
+              multiline
+              maxRows={3}
+              fullWidth
+              value={descripcionLista}
+              onChange={handleDescripcionChange}
+            />
+            <Typography variant="h5" sx={{ ...theme.normalText }} align="center">Color de la Lista:</Typography>
+            <FormControl fullWidth sx={{ ...theme.container }}>
+              <Select
+                value={colorLista}
+                onChange={handleColorChange}
+              >
+                <MenuItem value="red">Rojo</MenuItem>
+                <MenuItem value="blue">Azul</MenuItem>
+                <MenuItem value="green">Verde</MenuItem>
+                <MenuItem value="yellow">Amarillo</MenuItem>
+              </Select>
+            </FormControl>
+            <Box display="flex" justifyContent="space-between" style={{ marginTop: '30px', marginBottom: '15px' }}>
+              <Button variant="contained" color="primary" size="large" onClick={handleSubmit}>
+                Crear Lista de Tareas
+              </Button>
+              <Button variant="contained" color="error" size="large">
+                Cancelar
+              </Button>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 };
 
-export default listaTareas;
+export default ListaTareas;
