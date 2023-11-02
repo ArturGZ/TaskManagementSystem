f532fb5b1dd68eba62f94f9a9318b076114cb8e4
ArturGZ <arturo.quintana.016@gmail.com>
2023-11-01 22:50:12 -0600

    Added drop-down side menu , a menu is added from the user's avatar, useless files are cleaned and logs are added

--- login/app/home/page.jsx
+++ login/app/home/page.jsx
@@ -4,8 +4,9 @@ import { Grid, Button, Card, CardActionArea, CardMedia, CardContent, Typography 
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 
+
 export default function Home() {
-  const { status } = useSession();
+	const { data: session } = useSession();
   return (
     <Grid 
       container
@@ -13,55 +14,56 @@ export default function Home() {
       alignItems='center'
       marginY={8}
     >
-        <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', mx: 4, my:8 }} >
-				<Link href='/tasks' style={{textDecoration: 'none'}}>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" color='black'>
-                                Create Task
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Add new task to a task list, including fields for title,
-                                description, due date and status.
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-				</Link>
-            </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', mx: 4 }}>
-                <Link href='/task-list' style={{textDecoration: 'none'}}>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" color='black'>
-                                Task List
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Display a list of task lists created by you, allowing management of tasks.
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Link>
-            </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', mx: 4, my: 8 }}>
-                <Link href='/' style={{textDecoration: 'none'}}>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" color='black'>
-                                Create Task List
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Will be able to create new task lists, providing a name, and description and customizing the color of the list.
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Link>
-            </Card>
-        </Grid>
+			<Grid item xs={12} sm={6} md={4}>
+				<Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', mx: 4, my:8 }} >
+					{/* If user is authenticated go to correct page else go to login*/}
+					<Link href={session ? '/tasks': '/login'} style={{textDecoration: 'none'}}>
+						<CardActionArea>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="div" color='black'>
+										Create Task
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
+										Add new task to a task list, including fields for title,
+										description, due date and status.
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+					</Link>
+				</Card>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4}>
+				<Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', mx: 4 }}>
+					<Link href={session ? '/task-list': '/login'} style={{textDecoration: 'none'}}>
+						<CardActionArea>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="div" color='black'>
+									Task List
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
+									Display a list of task lists created by you, allowing management of tasks.
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+					</Link>
+				</Card>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4}>
+				<Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', mx: 4, my: 8 }}>
+					<Link href={session ? '/': '/login'} style={{textDecoration: 'none'}}>
+						<CardActionArea>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="div" color='black'>
+									Create Task List
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
+									Will be able to create new task lists, providing a name, and description and customizing the color of the list.
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+					</Link>
+				</Card>
+			</Grid>
     </Grid>
   );
 }
--- login/components/navbar.jsx
+++ login/components/navbar.jsx
@@ -1,43 +1,55 @@
 'use client';
 
-// Common navigation bar foe all modules
+// Common navigation bar for all modules
 
-import { AppBar, Box, Button, Toolbar, Typography, CssBaseline, Avatar} from '@mui/material';
-import { useSession, signOut } from 'next-auth/react';
+import { AppBar, Box, Button, Toolbar, Zoom, Typography, CssBaseline } from '@mui/material';
+import { Tooltip, CircularProgress  } from '@mui/material';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import AccountMenu from './account-menu';
+import DrawerMenu from './drawer-menu';
 
 export default function Navbar() {
+
     const { status, data:session } = useSession();
+		try{
+			const userPhoto = session?.user.image;
+		} catch (e){
+			console.warn('A problem occurred while obtaining the user photo');
+		}
+
     return (
 		<Box sx={{ flexGrow: 1 }}>
-			<CssBaseline />
+			<CssBaseline />	{/* Eliminate unwanted margins */}
+			
 			<AppBar position='static' sx={{ boxShadow: 'md' }} data-testid='appbar'>
-				<Toolbar sx= {{ justifyContent: 'space-between' }}data-testid='toolbar'>
-					<Link href='/'>
-						<Button variant='text' size='small' data-testid='btn-navbar-home'>
-							<Typography variant='h5' component='div' sx={{ flexGrow: 1, color: 'white' }} data-testid='name-app'>
-								Kairos
-							</Typography>
-						</Button>
-					</Link>
+				<Toolbar sx= {{ justifyContent: 'space-between' }} data-testid='toolbar'>
+
+					<Box alignItems='center'>
+						{/* For the side panel */}
+						<DrawerMenu />	
+
+						<Link href='/'>
+							<Tooltip title={'Kairos'} arrow TransitionComponent={Zoom}>
+								<Button variant='text' size='small' data-testid='btn-navbar-home'>
+									<Typography variant='h5' sx={{ flexGrow: 1, color: 'white' }} data-testid='name-app'>
+										Kairos
+									</Typography>
+								</Button>
+							</Tooltip>
+						</Link>
+					</Box>
+
+					{/* Dynamic part of navbar */}
 					{status === 'authenticated' ? (
-						<>
-						<Box sx={{flexDirection: 'row',display: 'flex',}}>
-							<Avatar alt={session?.user.name} src={session?.user.image}
-								sx={{
-									marginX: 2
-								}}
-								data-testid='image-user'
-							>
-							</Avatar>
-							<Button onClick={() => signOut()} variant='outlined' color='inherit' data-testid='btn-signout'>Sign Out</Button>
-						</Box>
-						</>
+						<AccountMenu />	// For account options from user avatar
+					) : status === 'loading' ? (
+						<CircularProgress color='inherit'/> // While getting the session state
 					) : (
 						<>
 						<Link href='/login' >
 							<Button variant='outlined' data-testid='btn-signin'>
-								<Typography variant='body2' component='div' sx={{ flexGrow: 1, color: 'white' }}>
+								<Typography variant='body2' sx={{ flexGrow: 1, color: 'white' }}>
 									Sign In
 								</Typography>
 							</Button>
