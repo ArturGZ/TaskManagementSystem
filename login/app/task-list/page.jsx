27f12f26d135bd40769180d7cde4f5b8e7d60465
Yoaddan <yokaem@gmail.com>
2023-11-05 06:56:26 -0600

    Changed data from constants to localStorage changed: now tasks lists are saved on localStorage feat: responsive elements added for main grid and add/delete buttons

--- login/app/task-list/page.jsx
+++ login/app/task-list/page.jsx
@@ -1,53 +1,40 @@
 'use client';
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../styles/theme';
-import TaskList from '../../components/task-list'
+import TaskList from '../../components/task-list';
 import DeleteButton from '../../components/delete-button';
 import AddButton from '../../components/add-button';
 import ToggleButton from '../../components/toggle-button';
-import { Grid, Typography} from '@mui/material';
+import { Grid, Typography, Container, Box } from '@mui/material';
 import Link from 'next/link';
 
 export default function Home() {
+  const isClient = typeof window !== 'undefined'; // Check if we are on the client
 
-   // Datos de ejemplo 
-   const initialTaskLists = [
-    { id: 1, name: 'Task List 1', description: 'Description List 1' , color: '#9A5343', tasks: [
-      {id: 1, name: 'Taks 1 of List 1', description:'Description Task 1', due: '10-11-2023', status: 'Incomplete'},
-      {id: 2, name: 'Task 2 of List 1', description:'Description Task 2', due: '10-11-2023', status: 'Incomplete'},
-      {id: 3, name: 'Task 3 of List 1', description:'Description Task 3', due: '10-11-2023', status: 'Incomplete'}
-    ]},
-    { id: 2, name: 'Task List 2', description: 'Description List 2' , color: 'blue', tasks: [
-      {id: 1, name: 'Taks 1 of List 2', description:'Description Task 1', due: '10-11-2023', status: 'Incomplete'},
-      {id: 2, name: 'Task 2 of List 2', description:'Description Task 2', due: '10-11-2023', status: 'Incomplete'},
-      {id: 3, name: 'Task 3 of List 2', description:'Description Task 3', due: '10-11-2023', status: 'Incomplete'}
-    ]},
-    { id: 3, name: 'Task List 3', description: 'Description List 3' , color: 'yellow', tasks: [
-      {id: 1, name: 'Taks 1 of List 3', description:'Description Task 1', due: '10-11-2023', status: 'Incomplete'},
-      {id: 2, name: 'Task 2 of List 3', description:'Description Task 2', due: '10-11-2023', status: 'Incomplete'},
-      {id: 3, name: 'Task 3 of List 3', description:'Description Task 3', due: '10-11-2023', status: 'Incomplete'}
-    ]},
-    { id: 4, name: 'Task List 4', description: 'Description List 4' , color: 'red', tasks: [
-      {id: 1, name: 'Taks 1 of List 4', description:'Description Task 1', due: '10-11-2023', status: 'Incomplete'},
-      {id: 2, name: 'Task 2 of List 4', description:'Description Task 2', due: '10-11-2023', status: 'Incomplete'},
-      {id: 3, name: 'Task 3 of List 4', description:'Description Task 3', due: '10-11-2023', status: 'Incomplete'}
-    ]},
-    { id: 5, name: 'Task List 5', description: 'Description List 5' , color: 'blue', tasks: [
-      {id: 1, name: 'Taks 1 of List 5', description:'Description Task 1', due: '10-11-2023', status: 'Incomplete'},
-      {id: 2, name: 'Task 2 of List 5', description:'Description Task 2', due: '10-11-2023', status: 'Incomplete'},
-      {id: 3, name: 'Task 3 of List 5', description:'Description Task 3', due: '10-11-2023', status: 'Incomplete'}
-    ]},
-  ];
+  // Data
+  const initialTaskLists = [];
 
-  // Estados
-  const [taskLists, setTaskLists]= useState(initialTaskLists);
+  // Function to get data from local storage
+  const getStoredData = () => {
+        const storedData = localStorage.getItem('taskLists');
+        return storedData ? JSON.parse(storedData) : initialTaskLists;
+    }
+  
+
+  // States
+  const [taskLists, setTaskLists] = useState(getStoredData());
   const [checkedItems, setCheckedItems] = useState([]);
-  const [showBigButtons, setShowBigButtons] = useState(true); 
+  const [showBigButtons, setShowBigButtons] = useState(true);
 
-  // Función para manejar las tareas seleccionadas
+  // Effect to save data to local storage when it changes
+  useEffect(() => {
+      localStorage.setItem('taskLists', JSON.stringify(taskLists));
+  }, [taskLists]);
+
+  // Function to handle checked task lists
   function handleCheckboxChange(taskListId) {
-    console.info('[INFO] Actualizando elementos seleccionados');
+    console.info('[INFO] Updating selected items');
     if (checkedItems.includes(taskListId)) {
       setCheckedItems(checkedItems.filter((id) => id !== taskListId));
       console.debug(checkedItems);
@@ -57,28 +44,29 @@ export default function Home() {
     }
   }
 
-  // Función para eliminar las listas seleccionadas
+  // Function to delete the checked task lists
   function handleDeleteButtonClick() {
     const updatedTaskLists = taskLists.filter((taskList) => !checkedItems.includes(taskList.id));
     setCheckedItems([]);
     setTaskLists(updatedTaskLists);
-    console.info('[INFO] Se han eliminado las listas seleccionadas')
+    console.info('[INFO] Selected lists have been deleted');
     console.debug(taskLists);
   }
 
-  // Función para obtener id siguiente de lista
+  // Function to obtain the new task list id
   const getNewTaskListId = () => {
     const existingIds = taskLists.map((taskList) => taskList.id);
     return existingIds.length > 0 ? Math.max(...existingIds) + 1 : 1;
-  };
+  }
 
+  // Function to obtain the new task id
   const getNewTaskId = (taskListId) => {
     const taskList = taskLists.find((list) => list.id === taskListId);
     const existingIds =  taskList.tasks.map((tasks) => tasks.id);
     return existingIds.length > 0 ? Math.max(...existingIds) + 1 : 1;
   }
 
-  // Función para añadir nuevas listas
+  // Function to add new task lists
   function handleAddButtonClick(){
     const newTaskListId = getNewTaskListId();
 
@@ -86,29 +74,29 @@ export default function Home() {
       id: newTaskListId,
       name: `Task List ${newTaskListId}`,
       description: `Description List ${newTaskListId}`,
-      color: 'red',
+      color: '#DE4035',
       tasks: []
     };
 
     setTaskLists([...taskLists, newTaskList]);
   }
 
-  // Función para añadir nuevas tareas
+  // Function to add new tasks
   function handleAddTaskClick(taskListId){
     const newTaskId = getNewTaskId(taskListId);
-  
+
     const newTask = {
       id: newTaskId,
-      name: `Tarea ${newTaskId} de la lista ${taskListId}`,
-      description:`Description Task ${newTaskId}`,
+      name: `Task ${newTaskId} of List ${taskListId}`,
+      description: `Description Task ${newTaskId}`,
       due: '25-12-2023',
-      status: 'Incomplete'
-    }
-  
-    // Encuentra la lista correspondiente por su id
+      status: 'In Progress'
+    };
+
+    // Find the corresponding list by its id
     const updatedTaskLists = taskLists.map((taskList) => {
       if (taskList.id === taskListId) {
-        // Añade la nueva tarea a la lista correspondiente
+        // Add the new task to the corresponding list
         return {
           ...taskList,
           tasks: [...taskList.tasks, newTask],
@@ -116,47 +104,40 @@ export default function Home() {
       }
       return taskList;
     });
-  
-    // Actualiza el estado con la nueva lista que contiene la tarea
+
+    // Update the state with the new list that contains the task
     setTaskLists(updatedTaskLists);
   }
-  
 
   return (
     <ThemeProvider theme={theme}>
-    <div>
-      <Typography style={{backgroundColor: '#bf34bb' ,  // Color de fondo
-        color: theme.palette.primary.contrastText,    // Color del texto
-        padding: theme.spacing(2),                    // Espaciado
-        textAlign: 'center',
-        fontSize: '200%'}}>
-        Tasks Lists
-      </Typography>
-      <Grid container spacing={{ xs: 0, sm: 1, md: 2 }} columns={{ xs: 2, sm: 3, md: 4 }}>
-        {taskLists.map((taskList) => (
-          <Grid item xs={1} sm={1} md={1} key={taskList.id}>
-            <TaskList
-              taskList={taskList}
-              handleCheckboxChange={handleCheckboxChange}
-              checkedItems={checkedItems}
-              handleAddTaskClick={handleAddTaskClick}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      {showBigButtons && (
-          <Grid item xs={1} sm={1} md={1}>
+      <Container>
+        <Typography variant="h2" style={{ backgroundColor: '#bf34bb', color: theme.palette.primary.contrastText, padding: theme.spacing(2), textAlign: 'center' }}>
+          Task Lists
+        </Typography>
+        <Grid container spacing={2}>
+          {taskLists && taskLists.map((taskList) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={taskList.id}>
+              <TaskList
+                taskList={taskList}
+                handleCheckboxChange={handleCheckboxChange}
+                checkedItems={checkedItems}
+                handleAddTaskClick={handleAddTaskClick}
+              />
+            </Grid>
+          ))}
+        </Grid>
+        {showBigButtons && (
+          <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: theme.spacing(2) }}>
             <DeleteButton onClick={() => handleDeleteButtonClick()} />
-            <Link href='/tasks' style={{textDecoration: 'none'}} >
+            <Link href="/tasks" style={{ textDecoration: 'none' }}>
               <AddButton onClick={() => handleAddButtonClick()} />
             </Link>
-          </Grid>
+          </Box>
         )}
-        <ToggleButton
-          onClick={() => setShowBigButtons(!showBigButtons)}
-          showBigButtons={showBigButtons}
-        />
-    </div>
+        <ToggleButton onClick={() => setShowBigButtons(!showBigButtons)} showBigButtons={showBigButtons} />
+      </Container>
     </ThemeProvider>
   );
 }
+
