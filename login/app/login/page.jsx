bb6c02510df20645ddf58030bda8e7f0ab2ac5df
ArturGZ <arturo.quintana.016@gmail.com>
2023-11-18 16:21:06 -0600

    Redirection on login/logout

--- login/app/login/page.jsx
+++ login/app/login/page.jsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { Grid, Button, Typography, CircularProgress, Box } from '@mui/material';
+import { Grid, CircularProgress, Box } from '@mui/material';
 import UserLogin from '../../components/user-login';
-import { useSession, signOut } from 'next-auth/react';
-import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation'
+
 
 export default function Login() {
   const { status } = useSession();
@@ -15,30 +16,15 @@ export default function Login() {
       style={{ height: '100vh', marginTop: '-24px' }}
     >
       {status === 'authenticated' ? (
-        <>
-        <Grid 
-          item xs={12} sm={6} md={4} 
-          container
-          justifyContent='center'
-          alignItems='center'
-          marginY={8}
-        >
-          <Button onClick={() => signOut()} color='inherit'>Sign Out</Button>
-          <Link href='/'>
-              <Button variant='cointained' color='primary' size='small' data-testid='btn-navbar-home'>
-                  <Typography variant='body2' component='div' sx={{ flexGrow: 1, color: 'Black' }} data-testid='name-app'>
-                      You are logged, Back to home
-                  </Typography>
-              </Button>
-          </Link>
-        </Grid>
-        </>
+          // Automatically redirects to home
+          redirect('/')
       ) : status === 'loading' ? (
         // While the session state is being updated
         <Box sx={{ flexGrow: 1, display: 'flex', alignContent: 'center', justifyContent:'center' }}>
           <CircularProgress color='primary'/>
         </Box>
       ) : (
+        // Login Form
         <UserLogin />
       )}
     </Grid>
